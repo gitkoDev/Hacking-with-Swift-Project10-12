94168f0a47e30b835555699f814d95ae459f76fb
gitkoDev <prostodenis1397@gmail.com>
2022-07-11 10:22:31 +0800

    Initial Commit

---
+++ Project10-12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project10-12
+//
+//  Created by Gitko Denis on 11.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
